2ee560838065d7c0892b78ab6e039970a6bcd99c
michaelcheers <michaelcheers8@gmail.com>
2015-01-24 17:28:00 -0800

    seperated to multiple code file.
    seperated to multiple code file.
    picture : picture.cs
    brodcastevent : brodcastevent.cs

--- Scratch/Library/BrodcastEvent.cs
+++ Scratch/Library/BrodcastEvent.cs
@@ -11,28 +11,6 @@ using Microsoft.Xna.Framework.Media;
 
 namespace Scratch
 {
-    namespace BonusContent
-    {
-    }
-    public class Picture
-    {
-        Texture2D Internal;
-        World world;
-        public Picture(System.IO.Stream stream, World world)
-        {
-            Internal = Texture2D.FromStream(world.game.GraphicsDevice, stream);
-            this.world = world;
-        }
-        public Picture(string path, World world)
-        {
-            Internal = Texture2D.FromStream(world.game.GraphicsDevice, System.IO.File.Open(path, System.IO.FileMode.Open));
-            this.world = world;
-        }
-        public Sprite ConvertToSprite()
-        {
-            return new Sprite(Internal, world);
-        }
-    }
     public class BrodcastEvent
     {
         List<System.Threading.ThreadStart> broadcast = new List<System.Threading.ThreadStart>();
---
+++ Scratch/Library/Picture.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Scratch
+{
+    public class Picture
+    {
+        Texture2D Internal;
+        World world;
+        public Picture(System.IO.Stream stream, World world)
+        {
+            Internal = Texture2D.FromStream(world.game.GraphicsDevice, stream);
+            this.world = world;
+        }
+        public Picture(string path, World world)
+        {
+            Internal = Texture2D.FromStream(world.game.GraphicsDevice, System.IO.File.Open(path, System.IO.FileMode.Open));
+            this.world = world;
+        }
+        public Sprite ConvertToSprite()
+        {
+            return new Sprite(Internal, world);
+        }
+    }
+}
